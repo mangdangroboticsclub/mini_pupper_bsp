03364fa4056ce2052f76451449ac2137ebd24681
Daisuke Sato <tiryoh@gmail.com>
2022-08-26 13:25:02 +0900

    Fix device file permission error

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selecti
 sudo sed -i "s/# deb-src/deb-src/g" /etc/apt/sources.list
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y i2c-tools dpkg-dev curl python-is-python3 mpg321 python3-tk
+sudo apt install -y i2c-tools dpkg-dev curl python-is-python3 mpg321 python3-tk openssh-server
 sudo sed -i "s/pulse/alsa/" /etc/libao.conf
 if [ $(lsb_release -cs) == "jammy" ]; then
     sudo sed -i "s/cards.pcm.front/cards.pcm.default/" /usr/share/alsa/alsa.conf
@@ -56,3 +56,24 @@ if [ $(lsb_release -cs) == "jammy" ]; then
     sudo sed -i "s/3-00500/3-00501/" $BASEDIR/Python_Modules/MangDang/minipupper/calibrate_servos.py
 fi
 sudo pip install $BASEDIR/Python_Modules
+
+### Make pwm sysfs and nvmem work for non-root users
+### reference: https://github.com/raspberrypi/linux/issues/1983
+getent group gpio || sudo groupadd gpio && sudo gpasswd -a $(whoami) gpio
+getent group dialout || sudo groupadd dialout && sudo gpasswd -a $(whoami) dialout
+sudo tee /etc/udev/rules.d/99-minipupper-pwm.rules << EOF > /dev/null
+SUBSYSTEM=="pwm*", PROGRAM="/bin/sh -c '\
+        chown -R root:gpio /sys/class/pwm && chmod -R 770 /sys/class/pwm;\
+        chown -R root:gpio /sys/class/pwm/pwmchip0/pwm*/duty_cycle && chmod -R 770 /sys/class/pwm/pwmchip0/pwm*/duty_cycle'"
+EOF
+
+if [ $(lsb_release -cs) == "jammy" ]; then
+sudo tee /etc/udev/rules.d/99-minipupper-nvmem.rules << EOF > /dev/null
+SUBSYSTEM=="nvmem", DEVPATH=="*0-00501", RUN+="/bin/chown root:gpio /sys$env{DEVPATH}/nvmem", RUN+="/bin/chmod 660 /sys$env{DEVPATH}/nvmem"
+EOF
+else
+sudo tee /etc/udev/rules.d/99-minipupper-nvmem.rules << EOF > /dev/null
+SUBSYSTEM=="nvmem", DEVPATH=="*0-00500", RUN+="/bin/chown root:gpio /sys$env{DEVPATH}/nvmem", RUN+="/bin/chmod 660 /sys$env{DEVPATH}/nvmem"
+EOF
+fi
+sudo udevadm control --reload-rules && sudo udevadm trigger
