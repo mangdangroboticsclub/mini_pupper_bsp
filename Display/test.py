4848323714c918128261f357285f0e0993545e4c
Horst Dumcke <hdumcke@marlysys.com>
2022-09-23 09:19:05 +0200

    Display now shows IP address, added placeholder images for different states

--- Display/test.py
+++ Display/test.py
@@ -1,20 +1,24 @@
-from PIL import Image
-from MangDang.LCD.ST7789 import ST7789
+from enum import Enum
+from MangDang.minipupper.display import Display
+
+
+class BehaviorState(Enum):
+    DEACTIVATED = -1
+    REST = 0
+    TROT = 1
+    HOP = 2
+    FINISHHOP = 3
+    SHUTDOWN = 96
+    IP = 97
+    TEST = 98
+    LOWBATTERY = 99
 
 
 def main():
-    """ The demo for picture show
+    """ Display a test picture
     """
-
-    # init st7789 device
-    disp = ST7789()
-    disp.begin()
-    disp.clear()
-
-    # show exaple picture
-    image = Image.open("./dog.png")
-    image.resize((320, 240))
-    disp.display(image)
+    disp = Display()
+    disp.show_state(BehaviorState.TEST)
 
 
 main()
