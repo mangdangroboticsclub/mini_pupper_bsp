bb53903a223e790f9964e27b0436d0f9fb953496
allenkwok11 <91792358+allenkwok11@users.noreply.github.com>
2026-03-13 14:25:11 +0800

    refactor: reuse 22.04 config.txt for Ubuntu 24.04 and remove redundant folder

--- IO_Configuration/install.sh
+++ IO_Configuration/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 UBUNTU_CODENAME=${UBUNTU_CODENAME:-$(lsb_release -cs)}
-if [ "$UBUNTU_CODENAME" == "jammy" ]; then
+if [ "$UBUNTU_CODENAME" == "jammy" ] || [ "$UBUNTU_CODENAME" == "noble" ]; then
     sudo cp ubuntu_22.04/config.txt /boot/firmware/ -f
-elif [ "$UBUNTU_CODENAME" == "noble" ]; then
-    sudo cp ubuntu_24.04/config.txt /boot/firmware/ -f
 else
     sudo cp ubuntu_20.04/syscfg.txt /boot/firmware/ -f
 fi
